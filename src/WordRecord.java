4c8d17264c67996bf522a8786ae25fbf19f5a994
David Pullinger <davidpullinger003@gmail.com>
2021-09-02 15:41:08 +0200

    cleaned up skeleton files

--- src/WordRecord.java
+++ src/WordRecord.java
@@ -1,70 +1,66 @@
-package skeletonCodeAssgnmt2;
-
 public class WordRecord {
 	private String text;
-	private  int x;
+	private int x;
 	private int y;
 	private int maxY;
 	private boolean dropped;
-	
+
 	private int fallingSpeed;
-	private static int maxWait=1500;
-	private static int minWait=100;
+	private static int maxWait = 1500;
+	private static int minWait = 100;
 
 	public static WordDictionary dict;
-	
 
-	
 	WordRecord() {
-		text="";
-		x=0;
-		y=0;	
-		maxY=300;
-		dropped=false;
-		fallingSpeed=(int)(Math.random() * (maxWait-minWait)+minWait); 
+		text = "";
+		x = 0;
+		y = 0;
+		maxY = 300;
+		dropped = false;
+		fallingSpeed = (int) (Math.random() * (maxWait - minWait) + minWait);
 	}
-	
+
 	WordRecord(String text) {
 		this();
-		this.text=text;
-	}
-	
-	WordRecord(String text,int x, int maxY) {
-		this(text);
-		this.x=x;
-		this.maxY=maxY;
-	}
-	
-// all getters and setters must be synchronized
-	public synchronized  void setY(int y) {
-		if (y>maxY) {
-			y=maxY;
-			dropped=true;
-		}
-		this.y=y;
-	}
-	
-	public synchronized  void setX(int x) {
-		this.x=x;
-	}
-	
-	public synchronized  void setWord(String text) {
-		this.text=text;
+		this.text = text;
 	}
 
-	public synchronized  String getWord() {
+	WordRecord(String text, int x, int maxY) {
+		this(text);
+		this.x = x;
+		this.maxY = maxY;
+	}
+
+	// all getters and setters must be synchronized
+	public synchronized void setY(int y) {
+		if (y > maxY) {
+			y = maxY;
+			dropped = true;
+		}
+		this.y = y;
+	}
+
+	public synchronized void setX(int x) {
+		this.x = x;
+	}
+
+	public synchronized void setWord(String text) {
+		this.text = text;
+	}
+
+	public synchronized String getWord() {
 		return text;
 	}
-	
-	public synchronized  int getX() {
+
+	public synchronized int getX() {
 		return x;
-	}	
-	
-	public synchronized  int getY() {
+	}
+
+	public synchronized int getY() {
 		return y;
 	}
-	
-	public synchronized  int getSpeed() {
+
+	public synchronized int getSpeed() {
 		return fallingSpeed;
 	}
 
@@ -72,35 +68,34 @@ public class WordRecord {
 		setY(y);
 		setX(x);
 	}
+
 	public synchronized void resetPos() {
 		setY(0);
 	}
 
 	public synchronized void resetWord() {
 		resetPos();
-		text=dict.getNewWord();
-		dropped=false;
-		fallingSpeed=(int)(Math.random() * (maxWait-minWait)+minWait); 
-		//System.out.println(getWord() + " falling speed = " + getSpeed());
+		text = dict.getNewWord();
+		dropped = false;
+		fallingSpeed = (int) (Math.random() * (maxWait - minWait) + minWait);
+		// System.out.println(getWord() + " falling speed = " + getSpeed());
 
 	}
-	
+
 	public synchronized boolean matchWord(String typedText) {
-		//System.out.println("Matching against: "+text);
+		// System.out.println("Matching against: "+text);
 		if (typedText.equals(this.text)) {
 			resetWord();
 			return true;
-		}
-		else
+		} else
 			return false;
 	}
-	
 
-	public synchronized  void drop(int inc) {
-		setY(y+inc);
+	public synchronized void drop(int inc) {
+		setY(y + inc);
 	}
-	
-	public synchronized  boolean dropped() {
+
+	public synchronized boolean dropped() {
 		return dropped;
 	}
 
